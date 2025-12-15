52814fb1cfde5f9862b15dfb292921b286db93a4
Ebenezer <ebenezereshetu@gmail.com>
2025-12-15 04:03:34 -0500

    Clean up repo: remove duplicate src/data, add .gitignore, fix exports

--- 2xeb-portfolio/src/data/index.ts
+++ 2xeb-portfolio/src/data/index.ts
@@ -4,3 +4,5 @@ export { EXPERIENCE } from './timeline';
 export { COLORS, GRAPH_DATA, generateGraph } from './graph';
 export { CASE_STUDIES, getCaseStudyBySlug } from './caseStudies';
 export type { CaseStudy, TimelineEvent } from './caseStudies';
+export { SITE_INDEX } from './siteIndex';
+export type { SitePage } from './siteIndex';
